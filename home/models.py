86a28b7cb3a1a37b754b79cbee3dc9d6f99c824b
ShinzoSin <72959752+ShinzoSin@users.noreply.github.com>
2021-04-13 22:59:22 +0100

    uploaded changes so far

---
+++ home/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+from wagtail.core.models import Page
+
+
+class HomePage(Page):
+    pass
+    
+class Asteroids(Page):
+    pass
+    
+class Contact(Page):
+    pass
+
+class About(Page):
+    pass
